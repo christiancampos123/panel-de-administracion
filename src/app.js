256dfb1076e779733faa7a5acc03fc8d88adccb6
christian.campos <christian.campos@192-168-1-187>
2023-12-04 15:43:54 +0100

    filter menu working

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ import './table.js';
 import './formButtons.js';
 import './image.js';
 import './modalDelete.js';
-import './modalFilter.js'
+// import './modalFilter.js'
 import './formValidation.js'
 import './title.js'
 import './menuHamburguer.js'
+import './filterModal.js'
