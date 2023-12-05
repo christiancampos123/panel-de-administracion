aca4c448e269deccccc3022df1d341f269b721d9
christiancampos123 <christiancampos1996@gmail.com>
2023-12-05 11:30:21 +0100

    finished component table

--- src/app.js
+++ src/app.js
@@ -2,9 +2,9 @@ import './filter.js';
 import './table.js';
 import './formButtons.js';
 import './image.js';
-// import './modalDelete.js';
 import './modalDestroy.js'
 import './formValidation.js'
 import './title.js'
 import './menuHamburguer.js'
 import './filterModal.js'
+import './tableRecords.js'
